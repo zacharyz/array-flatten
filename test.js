921a020f0c8910429278eebe2f97777f01d132d1
Zachary Zadell <zacharyz@gmail.com>
2019-10-20 16:59:42 -0700

    updated test description

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ test('flatten function exists', () => {
     expect(typeof flatten).toEqual('function');
 });
 
-test('flatten nested array', () => {
+test('flatten an already flattened array', () => {
     const flattened = flatten([1, 2, 3, 4, 5, 6]);
     expect(flattened).toEqual(expect.arrayContaining([1, 2, 3, 4, 5, 6]));
 });
